cee128b5ca353614a2695835ccfc9e9888503db5
Lucas Henderson <lucashendersonvieiracunha@gmail.com>
2023-02-13 16:03:39 -0300

    Usando ResponseEntity

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosDetalhamentoMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -30,26 +33,33 @@ public class MedicoController {
     
     @PostMapping //Declarando que esse metodo vai responder a um post
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) { //RequestBody faz com que a var receba o valor que chegar do endpoint
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) { //RequestBody faz com que a var receba o valor que chegar do endpoint
+        var medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping //não precisa do @Transactional, pois não estamos fazendo nenhuma alteração no bd, somente lendo
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
         //pega todos os medicos do bd, converte para DadosListagemMedico(um DTO) e cria uma lista
         //a paginação começa pela page 0        http://localhost:8080/medicos?size=1&page=0
         //http://localhost:8080/medicos?sort=nome ordena o get por nome crescente
         //http://localhost:8080/medicos?sort=nome,desc ordena o get por nome decrescente
 
         //return repository.findAll(paginacao).map(DadosListagemMedico::new);
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         Medico medico = repository.getReferenceById(dados.id());
         medico.AtualizarInformacoes(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
         //não precisa fazer mais nada, só de mudar os dados a JPA já identifica automaticamente e faz a atualização
     }
 
@@ -63,8 +73,18 @@ public class MedicoController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletar(@PathVariable Long id) {
+    public ResponseEntity deletar(@PathVariable Long id) {
         Medico medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build(); // devolve o cdg 204 que é o padrão para exclusão
+    }
+
+    
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        Medico medico = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 }
