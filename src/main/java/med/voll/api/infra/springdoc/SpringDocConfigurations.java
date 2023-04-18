347cc5f5621ca0aeedf3100a4a83b8d8a7de6446
Lucas Henderson <lucashenderson@unitins.br>
2023-04-18 15:56:38 -0300

    Documentação da API

---
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,24 @@
+package med.voll.api.infra.springdoc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SpringDocConfigurations {
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+            .components(new Components()
+            .addSecuritySchemes("bearer-key",
+            new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
+
+    //https://cursos.alura.com.br/course/spring-boot-3-documente-teste-prepare-api-deploy/task/122052
+}
+
+
